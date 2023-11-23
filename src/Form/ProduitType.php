e64854207bd516f905f7dec5f9a96ad44b914d05
unknown <elhadjoumartall10@gmail.com>
2023-11-23 11:17:26 +0100

    Cours symfony

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -11,13 +11,17 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface; 
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, ['constraints' => [ new NotBlank([ //ce code permet de valider le champ du formulaire defini
+                'message' => 'Nom requise']),
+                ],
+            ])
             ->add('price', MoneyType::class)
             ->add('description', TextareaType::class)
             ->add('quantity', IntegerType::class)
