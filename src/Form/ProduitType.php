ca81829722353e1bf528d794f497875fd9529ebd
unknown <elhadjoumartall10@gmail.com>
2023-11-23 11:54:28 +0100

    Cours symfony

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,26 +6,64 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface; 
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['constraints' => [ new NotBlank([ //ce code permet de valider le champ du formulaire defini
-                'message' => 'Nom requise']),
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Nom requis'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 255,
+                        'minMessage' => 'Le nom du produit est trop court',
+                        'maxMessage' => 'Le nom du produit est trop long',
+                    ]),
                 ],
             ])
-            ->add('price', MoneyType::class)
-            ->add('description', TextareaType::class)
-            ->add('quantity', IntegerType::class)
-            ->add('image', TextType::class);
+            ->add('price', MoneyType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le prix est requis'
+                    ]),
+                    new Positive([
+                        'message' => 'Le montant doit être positif'
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La description est requise'
+                    ]),
+                ],
+                'invalid_message' => 'Le prix doit toujours être en chiffre'
+            ])
+            ->add('quantity', IntegerType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'La quantité est requise'
+                    ]),
+                ],
+            ])
+            ->add('image', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Télécharger une image est requis'
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
